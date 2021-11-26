3ed713f4733a3a2eecb6984c5e0d0c414ae816df
clementejose <clemente.jac@gmail.com.com>
2021-11-26 01:34:32 -0300

    tox docs style

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,8 +2,7 @@ import os
 
 from pycellid.core import CellData
 
-import pytest as pt
-
+#import pytest as pt
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 base = os.path.dirname(ROOT_DIR)
@@ -11,4 +10,5 @@ file_path = os.path.join(base, 'samples_cellid')
 
 def test_cell_data():
     df = CellData(file_path)
-    assert not '_df' in vars(df)
+    df.path
+    assert '_df' in vars(df)

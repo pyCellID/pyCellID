c2e81d92e42989f1476b90bb836c3bab3f1af1a9
clementejose <clemente.jac@gmail.com.com>
2021-11-28 09:53:58 -0300

    core.py persiste operaciones sin salir del objeto

--- pycellid/core.py
+++ pycellid/core.py
@@ -20,7 +20,6 @@
 # =============================================================================
 # IMPORTS
 # =============================================================================
-
 import warnings
 from pathlib import Path
 
@@ -74,6 +73,8 @@ class CellData(object):
 
     path = attr.ib(validator=attr.validators.instance_of(str))
 
+    input = attr.ib(init=False)
+
     # : Starting with ``*`` will perform a recursive search on each string.
     name_data = attr.ib(
         validator=attr.validators.instance_of(str), default="out_all"
@@ -84,12 +85,22 @@ class CellData(object):
         validator=attr.validators.instance_of(str), default="*mapping"
     )
 
-
     @path.validator
     def _check_path(self, attribute, value):
         if not Path(value).exists():
             raise FileNotFoundError(f"Path < {value} > not exist")
 
+    @input.validator
+    def _check_input(self, attribute, value):
+        if (
+            isinstance(value, pd.DataFrame)
+            or isinstance(value, pd.Series)
+            or isinstance(value, self)
+        ):
+            pass
+
+        else:
+            raise ValueError
 
     @property
     def df(self):
@@ -98,13 +109,17 @@ class CellData(object):
         This property cosntruct the data tables.
         """
         if "_df" not in vars(self):
-            self._df = merge_tables(
-                path=self.path,
-                n_data=self.name_data,
-                n_mdata=self.name_meta_data,
-            )
+            if isinstance(input, pd.DataFrame) or isinstance(input, pd.Series):
+                self._df = input
+            elif isinstance(input, self.__class__):
+                self.to_pandas()
+            else:
+                self._df = merge_tables(
+                    path=self.path,
+                    n_data=self.name_data,
+                    n_mdata=self.name_meta_data,
+                )
         return self._df.copy()
-
 
     @property
     def plot(self):
@@ -114,6 +129,30 @@ class CellData(object):
         """
         return CellsPloter(self)
 
+    def to_pandas(self):
+
+        df = pd.DataFrame(data=self.df.values, columns=self.df.columns)
+        return df
+
+    #
+    def __call__(self, call, **kwargs):
+        """
+        When the instance is “called” as a function.
+
+        ``plot() <==> plot.__call__()``.
+        """
+        if call.startswith("_"):
+            raise AttributeError(f"Ivalid plot method '{call}'")
+
+        method = getattr(self, call, None)
+
+        if not callable(method):
+            raise AttributeError(f"Ivalid plot method '{call}'")
+
+        if method is None:
+            method = getattr(self.df, call)
+
+        return method(**kwargs)
 
     def __getattr__(self, a):
         """Is called when the default attribute access fails (AttributeError).
@@ -122,40 +161,29 @@ class CellData(object):
         """
         return getattr(self.df, a)
 
-
-    def __getitem__(self, k):
-        """Call to implement evaluation of self[key].
-
-        x[k] <=> x.__getitem__(k).
-        """
-        return self.df.__getitem__(k)
-
-    # def __getitem__(self, slice):
-    #     """x[y] <==> x.__getitem__(y)."""
-    #     sliced = self.df.__getitem__(slice)
-    #     return CellData(
-    #         model=self.model,
-    #         model_df=sliced,
-    #         plot_cls=self.plot_cls,
-    #         metadata=dict(self.metadata),
-    #     )
-
+    def __getitem__(self, slice):
+        """x[y] <==> x.__getitem__(y)."""
+        # import ipdb; ipdb.set_trace()
+        sliced = self.df.__getitem__(slice)
+        if not isinstance(sliced, self.__class__) or not isinstance(
+            sliced, pd.DataFrame
+        ):
+            return sliced
+        return CellData(input=sliced)
 
     def __iter__(self):
         """Call when an iterator is required for a container.
 
         iter(x) <=> x.__iter__().
         """
-        return iter(self.df)
-
+        return iter(self._df)
 
     def __len__(self):
         """Call to implement the built-in function len().
 
         len(x) <=> x.__len__().
         """
-        return len(self.df)
-
+        return len(self._df)
 
     def __repr__(self):
         """Compute the “official” string representation of an object.
@@ -164,18 +192,16 @@ class CellData(object):
         """
         with pd.option_context("display.show_dimensions", False):
             idx = [0, 1, 3, 4, -4, -3, -2, -1]
-            df_body = repr(self.df.iloc[idx,idx]).splitlines()
+            df_body = repr(self.df.iloc[idx, idx]).splitlines()
         df_dim = list(self.df.shape)
         sdf_dim = f"{df_dim[0]} rows x {df_dim[1]} columns"
 
         fotter = f"\nPyCellID.CellData - {sdf_dim}"
-        cell_data_repr = "\n".join(df_body +[fotter])
+        cell_data_repr = "\n".join(df_body + [fotter])
         return cell_data_repr
 
-
     def _repr_html_(self):
-        """Print a rich HTML version of your object.
-        """
+        """Print a rich HTML version of your object."""
         ad_id = id(self)
 
         with pd.option_context("display.show_dimensions", False):
@@ -184,10 +210,10 @@ class CellData(object):
         rows = f"{self.df.shape[0]} rows"
         columns = f"{self.df.shape[1]} columns"
 
-        footer = f"PyCellID.CellData - {rows} x {columns}"
+        footer = f"PyCellID.core.CellData - {rows} x {columns}"
 
         parts = [
-            f'<div class="PyCellID.CellData" id={ad_id}>',
+            f'<div class="PyCellID.core.CellData" id={ad_id}>',
             df_html,
             footer,
             "</div>",
@@ -196,10 +222,8 @@ class CellData(object):
         html = "".join(parts)
         return html
 
-
     def __setitem__(self, key, values):
-        """Call to implement assignment to self[key].
-        """
+        """Call to implement assignment to self[key]."""
         self._df[key] = values
 
 
@@ -231,6 +255,32 @@ class CellsPloter:
 
     cells = attr.ib()
 
+    def __call__(self, kind="cells_image", **kwargs):
+        """
+        When the instance is “called” as a function.
+
+        ``plot() <==> plot.__call__()``.
+        """
+        if kind.startswith("_"):
+            raise AttributeError(f"Ivalid plot method '{kind}'")
+
+        method = getattr(self, kind, None)
+
+        if not callable(method):
+            raise AttributeError(f"Ivalid plot method '{kind}'")
+
+        if method is None:
+            method = getattr(self.cells.df.plot, kind)
+
+        return method(**kwargs)
+
+    def __getattr__(self, a):
+        """
+        Is called when the default attribute access fails (AttributeError).
+
+        getattr(x, y) <==> x.__getattr__(y) <==> getattr(x, y).
+        """
+        return getattr(self.cells.df.plot, a)
 
     def __repr__(self):
         """
@@ -239,37 +289,6 @@ class CellsPloter:
         repr(x) <=> x.__repr__().
         """
         return f"CellsPloter(cells={hex(id(self.cells))})"
-
-
-    def __call__(self, kind="cells_image", **kwargs):
-        """
-        When the instance is “called” as a function.
-
-        type(x).__call__(x, arg1, ...).
-        """
-        method = getattr(self, kind, None)
-        if method is None:
-            method = getattr(self.cells._df.plot, kind)
-        return method(**kwargs)
-
-
-    def __getattr__(self, a):
-        """
-        Is called when the default attribute access fails (AttributeError).
-
-        getattr(x, y) <==> x.__getattr__(y) <==> getattr(x, y).
-        """
-        return getattr(self.cells._df.plot, a)
-
-
-    def __getitem__(self, k):
-        """
-        Call to implement evaluation of self[key].
-
-        x[k] <=> x.__getitem__(k).
-        """
-        return self.cells.df.__getitem__(k)        
-
 
     def cells_image(self, array_img_kws=None, imshow_kws=None, ax=None):
         """Representation of a set of cells.
@@ -307,7 +326,6 @@ class CellsPloter:
         ax.imshow(arr_c, **imshow_kws)
         ax.axis("off")
         return ax
-
 
     def cimage(self, idtfer, box_img_kws=None, imshow_kws=None, ax=None):
         """Representation of a sigle cell or image.

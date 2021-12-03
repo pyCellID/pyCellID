71864c6db39565b1c4c9b5c3590c9f33138e4b5e
clementejose <clemente.jac@gmail.com.com>
2021-12-03 09:29:00 -0300

    test_core.py np.testing rtol = 1e-01, verbose=True idx [2:] set ucid, non idx.copy() path copy

--- tests/conftest.py
+++ tests/conftest.py
@@ -132,9 +132,10 @@ def fake_filepath():
 def create_test_object_minimum():
     ucid = 100000000000
     df = CellData.from_csv(file_path)
+    path = df._path
     df = CellData(df._path, df[df.ucid==ucid].head(3)).copy()
     df = df[['pos', 't_frame', 'cellID', 'f_local2_bg_rfp', 'f_local2_bg_tfp']]
-    df = CellData(df._path, df)
+    df = CellData(path, df).copy()
     return df.copy()
 
 
